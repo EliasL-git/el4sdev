5d2d4fd05207fb868d53e5b80f4092b1116b4582
Elias <elias.lindholm2010@outlook.com>
2026-05-25 14:51:56 +0200

    Set Vite base to relative paths for subpath hosting

---
+++ src/pages/Freexyz.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './Freexyz.module.css'
+
+export default function Freexyz() {
+  return (
+    <main className={styles.page}>
+      <p className={styles.message}>
+        Build something cool! Then get a domain name from el4s / gen.xyz!!! (unofficial Hackclub program) DM me on hackclub
+        slack @
+        <a href="https://hackclub.enterprise.slack.com/team/U08J9R1TUT1">
+          https://hackclub.enterprise.slack.com/team/U08J9R1TUT1
+        </a>{' '}
+        to claim yours!
+      </p>
+    </main>
+  )
+}
