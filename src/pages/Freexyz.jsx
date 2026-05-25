7f7512244f5491c7ba762374a944baaa5c8233c4
Elias <elias.lindholm2010@outlook.com>
2026-05-25 14:43:55 +0200

    Add unofficial Hackclub program note to /freexyz copy

--- src/pages/Freexyz.jsx
+++ src/pages/Freexyz.jsx
@@ -5,7 +5,7 @@ export default function Freexyz() {
   return (
     <main className={styles.page}>
       <p className={styles.message}>
-        Build something cool! Then get a domain name from el4s / gen.xyz!!! DM me on hackclub
+        Build something cool! Then get a domain name from el4s / gen.xyz!!! (unoficcial Hackclub program) DM me on hackclub
         slack @
         <a href="https://hackclub.enterprise.slack.com/team/U08J9R1TUT1">
           https://hackclub.enterprise.slack.com/team/U08J9R1TUT1
